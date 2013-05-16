61475377187e12ee4a4747a4ae56df41d8b9f524
alcaras <alcaras@subcreation.net>
2013-05-16 15:01:44 -0400

    added suppress_count for to-read

--- sauce.py
+++ sauce.py
@@ -18,9 +18,12 @@ def print_books(books):
         print str(b.title[0:49]).ljust(49), str(b.isbn_13).rjust(13)
         
 
-def display_books(heading, query_results, limit=7):
+def display_books(heading, query_results, limit=7, suppress_count=False):
     print
-    print heading + " (" + str(len(query_results)) + ")"
+    if not suppress_count:
+        print heading + " (" + str(len(query_results)) + ")"
+    else:
+        print heading
     print dashes
     print_books(query_results[0:limit])
 
@@ -53,7 +56,8 @@ display_books("physical books that i want to read",
 books_unowned_to_read = session.query(Book).filter(Book.owned==False, Book.status=="To Read").order_by(Book.bumps.desc(), Book.date_added.asc()).all()
 
 display_books("books i want to read and do not own",
-              books_unowned_to_read)
+              books_unowned_to_read,
+              suppress_count=True)
 
 print
 
