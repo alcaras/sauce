e95d8e1d7e3c23a6bd8c9c9cc98a652635321e9d
alcaras <alcaras@subcreation.net>
2013-07-08 16:32:50 -0400

    added counts by month

--- sauce.py
+++ sauce.py
@@ -4,6 +4,8 @@ import pdb
 import sys
 
 import datetime
+import calendar
+from dateutil.relativedelta import relativedelta
 
 import pprint
 pp = pprint.PrettyPrinter(indent = 4)
@@ -13,6 +15,47 @@ from db import session
 from models import Book
 
 dashes="-"*80
+
+def pretty_counts(when, counts):
+    year = when.year
+    month = when.month
+    tag = str(year) + "-" + str(month)
+    n = 0
+    if tag in counts:
+        n = counts[tag]
+    label = str(calendar.month_name[month]).rjust(12) + " " + str(year)
+    print str(label).ljust(15), 
+    stars = ""
+    for i in range(0, n):
+        stars += "* "
+    print str(stars).ljust(25), str(n).rjust(3)
+
+
+    
+
+
+def month_analysis(books_read):
+    # let's go back through the last n months
+    counts = {}
+    for b in books_read:
+        if b.date_read is not None:
+            tag = str(b.date_read.year) + "-" + str(b.date_read.month)
+            if tag in counts:
+                counts[tag] += 1
+            else:
+                counts[tag] = 1
+
+    now = datetime.datetime.now()
+    
+
+    print "books read in past months"
+    print dashes
+    for less in range(0, 14):
+        pretty_counts(now+relativedelta( months = -less ), counts)
+
+    
+
+    
 
 def print_books(books, read=False):
     for b in books:
@@ -24,7 +67,7 @@ def print_books(books, read=False):
         print str(b.title[0:49]).ljust(49), str(b.isbn_13).rjust(13)
         
 
-def display_books(heading, query_results, limit=7, suppress_count=False, read=False):
+def display_books(heading, query_results, limit=10, suppress_count=False, read=False):
     print
     if not suppress_count:
         print heading + " (" + str(len(query_results)) + ")"
@@ -38,6 +81,8 @@ print "sauce book manager"
 print dashes
 
 books_read = session.query(Book).filter(Book.status=="Read").order_by(Book.date_read.desc()).all()
+
+month_analysis(books_read)
 
 display_books("books i've recently read", books_read, read=True)
 
