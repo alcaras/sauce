91686b5374e9599d55d9ea6a85f043ac68e9a5d7
alcaras <alcaras@subcreation.net>
2013-09-01 22:05:57 -0400

    unicode error fix

--- sauce.py
+++ sauce.py
@@ -64,7 +64,8 @@ def print_books(books, read=False):
             print str(b.date_added)[0:10].rjust(12),
         else:
             print str(b.date_read)[0:10].rjust(12),
-        print str(b.title[0:49]).ljust(49), str(b.isbn_13).rjust(13)
+        unicode_happy = str((b.title).encode('utf-8')[0:49]).ljust(49)
+        print unicode_happy, str(b.isbn_13).rjust(13)
         
 
 def display_books(heading, query_results, limit=10, suppress_count=False, read=False):
@@ -96,14 +97,14 @@ display_books("books i'm reading", books_reading)
 
 books_on_kindle_to_read = session.query(Book).filter(Book.owned==True, Book.kindle==True, Book.status=="To Read").order_by(Book.bumps.desc(), Book.date_added.asc()).all()
 
-display_books("books on kindle that i want to read",
+display_books("books on kindle i own that i want to read",
               books_on_kindle_to_read)
 
 #
 
 books_physical_to_read = session.query(Book).filter(Book.owned==True, Book.kindle==False, Book.status=="To Read").order_by(Book.bumps.desc(), Book.date_added.asc()).all()
 
-display_books("physical books that i want to read",
+display_books("physical books i own that i want to read",
               books_physical_to_read)
 
 #
